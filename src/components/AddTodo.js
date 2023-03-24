d67236801e7c31638ef75bbde6fc3a119164b68d
icika-max <icikakennedy@gmail.com>
2023-03-24 07:25:11 +0300

    adding deliverables

---
+++ src/components/AddTodo.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react';
+
+export default function AddTodo() {
+    const [allTodos, setTodos]=useState([]);
+    const  [newTitle,setNewTitle]=useState('')
+    const [newDescription,setNewDescription]=('')
+  return (
+    <div>AddTodo</div>
+  )
+}
